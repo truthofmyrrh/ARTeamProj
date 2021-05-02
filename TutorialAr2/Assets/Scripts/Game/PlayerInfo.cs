3bf4dc8c57250c5f2a1ad240b0a5099c46bc19a9
Sonak0930 <woals3850@gmail.com>
2021-05-02 13:28:50 +0900

    2020.3.61f Version
    set the inputsystem and ar foundation into MainSource

--- TutorialAr2/Assets/Scripts/Game/PlayerInfo.cs
+++ TutorialAr2/Assets/Scripts/Game/PlayerInfo.cs
@@ -9,32 +9,26 @@ public class PlayerInfo : MonoBehaviour
     public float coin = 0;
 
     private GameObject canvas;
-    private GameUI ui;
+    //private GameUI ui;
     
     
 
     void Start()
     {
         canvas = GameObject.Find("Canvas");
-        ui = canvas.GetComponent<GameUI>();
-        ui.UpdateStat();
-        if (ui == null)
-        {
-            Debug.Log("Could not get a ui scripts");
-            Application.Quit();
-        }
+        
     }
 
     public void ChangeHealth(float value)
     {
         health = value;
-        ui.UpdateStat();
+        
     }
 
     public void ChangeCoin(float value)
     {
         coin = value;
-        ui.UpdateStat();
+       
     }
     // Update is called once per frame
     void Update()
