cb11b06369539dec4bd158d21400f31c6415decb
Sonak0930 <woals3850@gmail.com>
2021-05-04 23:40:23 +0900

    Rollback
    Rollback to origin . (contains some erros.)

---
+++ TutorialAR/Assets/Scripts/Game/GameUI.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+public class GameUI : MonoBehaviour
+{
+	private float maxhealth = 0;
+	private float currenthealth = 0;
+	private float boostHealthValue = 1;
+	private List<GameObject> health;
+
+	private string FailedScene = "Result_fail";
+
+
+	private GameObject uiCanvas;
+	// Start is called before the first frame update
+	public void Start()
+    {
+		health = new List<GameObject>();
+		
+		uiCanvas = GameObject.Find("UIManager").transform.GetChild(1).gameObject;
+		CalculateHealth();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void SubstractHealth()
+	{
+		if(currenthealth == 0)
+        {
+			SceneManager.LoadScene(FailedScene);
+			return;
+        }
+		currenthealth--;
+		health[(int)currenthealth].SetActive(false);
+		//health.RemoveAt((int)currenthealth);
+	}
+
+	public void boosthealth()
+	{
+		if (health[(int)currenthealth].activeInHierarchy)
+		{
+			currenthealth++;
+		}
+
+		health[(int)currenthealth].SetActive(true);
+	}
+
+
+	public void CalculateHealth()
+	{
+		int i = 0;
+		GameObject h;
+
+        try
+        {
+			while( uiCanvas.transform.GetChild(5).transform.GetChild(i) != null)
+			{
+				h = uiCanvas.transform.GetChild(5).GetChild(i).gameObject;
+				health.Add(h);
+				maxhealth++;
+				Debug.Log(h.activeInHierarchy);
+				if (h.activeInHierarchy)
+				{
+					currenthealth++;
+				}
+
+				i++;
+			}
+        }
+
+		catch(Exception ex)
+        {
+			Debug.Log("Max hp " + maxhealth);
+			Debug.Log("cur hp " +currenthealth);
+
+			return;
+        }
+		
+		
+	}
+}
